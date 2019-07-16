05eba4584b274b322e4c67d74b5626f13c32c2a6
Fredrik Wiklund <wikan_f@hotmail.com>
2019-07-16 16:00:59 +0200

    Fixed issue when linking issues togheter. Added support to change shapes for epics. Added support for reading username from OS. Cleaned code on some places

--- main.py
+++ main.py
@@ -7,41 +7,36 @@ def die(message):
 
 import optparse
 import json
-from getpass import getpass
+import getpass
 
 try:
     import pygraphviz as pgv
 except ImportError:
-    die("Please install pygraphviz (in debian: python-pygraphviz)")
+    die("Please install pygraphviz (in debian: python3-pygraphviz)")
 try:
     import requests
     from requests.auth import HTTPBasicAuth
 except ImportError:
-    die("Please install restkit (in debian: python-requests)")
+    die("Please install restkit (in debian: python3-requests)")
 
 def fetcher_factory(server_url, auth):
     """ This factory will create the actual method used to fetch issues from JIRA. This is really just a closure that saves us having
         to pass a bunch of parameters all over the place all the time. """
     def get_issue(key):
-        """ Given an issue key (i.e. DEMO-1) return the JSON representation of it. This is the only place where we deal
-            with JIRA's REST API. """
+        """ Given an issue key (i.e. DEMO-1) return the JSON representation of it. """
         print('Fetching ' + key)
 
-        full_url = server_url + "/rest/agile/1.0/issue/" + key
-        response = requests.get(url=full_url,auth=auth,headers={"Accept": "application/json"})
+        request_url = server_url + "/rest/agile/1.0/issue/" + key
+        response = requests.get(url=request_url,auth=auth,headers={"Accept": "application/json"})
         
         if response.status_code == 200:
-            # Not all resources will return 200 on success. There are other success status codes. Like 204. We've read
-            # the documentation for though and know what to expect here.
-            issue = json.loads(response.text)
-            #print(json.dumps(issue, indent=4, sort_keys=True))
-            #print(issue["fields"]["issuelinks"])
-            return issue
+            #print(json.dumps(json.loads(response.text), indent=4, sort_keys=True))
+            return json.loads(response.text)
         else:
             return None
     return get_issue
-
-def add_dependencies_to_graph(graph, start_issue_key, get_issue):
+    
+def add_dependencies_to_graph(graph, get_issue):
     """ Given a starting issue key and the issue-fetching function build up the GraphViz data representing relationships
         between issues. This will consider both subtasks and issue links.
     """
@@ -61,38 +56,37 @@ def add_dependencies_to_graph(graph, start_issue_key, get_issue):
         
         seen.append(issue_key)
         children = []
-        fields = issue['fields']
-        
-       # if fields.get("epic"):
-        #    print("FIRST")
-         #   epickey = fields["epic"]["key"]
-          #  if (epickey, get_key(issue)) not in graph.edges():
-           #     graph.add_edge(epickey, get_key(issue), color='violet', label='epic', penwidth='1.5')
-            #    children.append(epickey)
-        if 'subtasks' in fields:
-            print("SECOND")
-            for subtask in issue['fields']['subtasks']:
-                if (get_key(issue), get_key(subtask)) not in graph.edges():
-                    graph.add_edge(get_key(issue), get_key(subtask), color='blue', label='subtask', penwidth='0.5')
-                children.append(subtask['key'])
-        if 'issuelinks' in fields:
-            print("THIRD")
-            for link in issue['fields']['issuelinks']:
-                print("liiinks")
-                if 'outwardIssue' in link:
-                    if (get_key(issue), get_key(link['outwardIssue'])) not in graph.edges():
-                        graph.add_edge(get_key(issue), get_key(link['outwardIssue']), label=link['type']['outward'])
-                        children.append(link['outwardIssue']['key'])
-                if 'inwardIssue' in link:
-                    if (get_key(link['inwardIssue']), get_key(issue)) not in graph.edges():
-                        graph.add_edge(get_key(link['inwardIssue']), get_key(issue), label=link['type']['outward'])
-                        children.append(link['inwardIssue']['key'])
-        # now construct graph data for all subtasks and links of this issue
+
+        # Add each linked issue we can find to the graph
+        for link in issue['fields']['issuelinks']:
+            if 'outwardIssue' in link:
+                if (get_key(issue), get_key(link['outwardIssue'])) not in graph.edges():
+                    graph.add_node(get_key(link['outwardIssue']), style='filled', label=get_key(link['outwardIssue']) + "\n" + link['outwardIssue']['fields']['summary'], penwidth='2.0')
+                    graph.add_edge(get_key(issue), get_key(link['outwardIssue']), label=link['type']['outward'])
+                    children.append(get_key(link['outwardIssue']))
+            if 'inwardIssue' in link:
+                if (get_key(link['inwardIssue']), get_key(issue)) not in graph.edges():
+                    graph.add_node(get_key(link['inwardIssue']), style='filled', label=get_key(link['inwardIssue']) + "\n" + link['inwardIssue']['fields']['summary'], penwidth='2.0')
+                    graph.add_edge(get_key(link['inwardIssue']), get_key(issue), label=link['type']['outward'])
+                    children.append(get_key(link['inwardIssue']))
+        # now construct graph data for all links of this issue
         for child in (x for x in children if x not in seen):
             walk(child, graph)
         return graph
-    
-    graph = walk(start_issue_key, graph)
+
+    # Starting building the graph recursively with the first element in out graph
+    graph = walk(graph.nodes()[0], graph)
+    return graph
+
+# Since it doesn't appear to be possible to determine the type of an issue based on the metadata, we utilizes the API to attempt to fetch an epic.
+# If it returns 200, it is an epic and the shape should be updated, if it returns 40X it is not an epic and we will leave it be.
+# https://developer.atlassian.com/cloud/jira/software/rest/#api-rest-agile-1-0-epic-epicIdOrKey-get
+def update_shape_on_epics(graph, server_url, auth):
+    for node in graph:
+        request_url = server_url + "/rest/agile/1.0/epic/" + node
+        response = requests.get(url=request_url,auth=auth,headers={"Accept": "application/json"})
+        if response.status_code == 200:
+            node.attr['shape'] = 'folder'
     return graph
 
 # Returns all issues that belong to the epic, for the given epic ID.
@@ -102,8 +96,7 @@ def get_issues_in_epic(server_url, epic_key, auth):
     response = requests.get(url=request_url,auth=auth,headers={"Accept": "application/json"})
     #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
     if response.status_code == 200:
-        jsonObject = json.loads(response.text)
-        return jsonObject
+        return json.loads(response.text)
     else:
         return None 
 
@@ -142,11 +135,11 @@ def create_graph_image(graph, image_file):
 
 def parse_args():
     parser = optparse.OptionParser()
-    parser.add_option('-u', '--user', dest='user', default='fredrik', help='Username to access JIRA')
+    parser.add_option('-u', '--user', dest='user', default=getpass.getuser(), help='Username to access JIRA')
     parser.add_option('-p', '--password', dest='password', help='Password to access JIRA')
     parser.add_option('-j', '--jira', dest='jira_url', default='http://localhost:8080', help='JIRA Base URL')
     parser.add_option('-f', '--file', dest='image_file', default='issue_graph.png', help='Filename to write image to')
-    parser.add_option('-v', '--verbose', dest='verbose', default=True, help='Adds all tasks and sub-tasks to graph')
+    parser.add_option('-v', '--verbose', dest='verbose', default=False, help='Adds all tasks that belongs to starting epic to graph')
     
     return parser.parse_args()
 
@@ -155,7 +148,7 @@ def get_password():
 
 if __name__ == '__main__':
     (options, args) = parse_args()
-
+ 
     # Basic Auth is usually easier for scripts like this to deal with than Cookies.
     auth = HTTPBasicAuth(options.user, options.password or get_password())
     issue_fetcher = fetcher_factory(options.jira_url, auth)
@@ -168,10 +161,12 @@ if __name__ == '__main__':
     graph = pgv.AGraph(strict=False, directed=True)
     graph.graph_attr['label'] = 'Dependency Graph for %s' % start_issue_key
     graph.add_node(start_issue_key, style='filled', shape='folder', penwidth='2.0')
-   
-    
-    # Get all dependencies to the graph, START_ISSUE CAN BE REMOVED, TAKE NAME FROM GRAPH OBJECT
-    graph = add_dependencies_to_graph(graph,start_issue_key, issue_fetcher)
+ 
+    # Get all dependencies to the graph
+    graph = add_dependencies_to_graph(graph, issue_fetcher)
+
+    # Since an issue could be linked to an EPIC (instead of belonging to) we want to distinguish does.
+    graph = update_shape_on_epics(graph, options.jira_url, auth)
 
     # Check if full issue tree shall be added to graph
     if options.verbose == True:
@@ -183,3 +178,12 @@ if __name__ == '__main__':
         
     # Create a picture from graph
     create_graph_image(graph, options.image_file)
+
+    # TODO
+    # Add support for summary in input epic
+    # Add verbose for all epics not only starting epic
+    # Add support for subgraphs to make the output abit clearer when using --verbose.
+    # Add support for keys so jenkins can use it?
+
+    # Funderingar, hur gör man detta snyggt egentligen? Känns som man får slänga med massor inloggningsuppgifter och URL överallt för att kunna hämta ärenden i varje funktion.
+    
